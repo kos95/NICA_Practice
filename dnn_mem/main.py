52596031976aea6d26d760b0c76f9652db4be578
kos <kos9502@gmail.com>
2019-02-14 22:55:47 +0900

    loss not go down

--- dnn_mem/main.py
+++ dnn_mem/main.py
@@ -1,21 +1,24 @@
 import torch
 import torch.nn as nn
 import os
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torch.autograd import Variable
 import torchvision.transforms as transforms
 import torchvision.datasets
-from bokeh.plotting import figure
-from bokeh.io import show
-from bokeh.models import LinearAxis, Range1d
-import numpy as np
 
-from skimage import io
+import numpy as np
+import skimage
 from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+from tifffile import imsave
+from sklearn.metrics import adjusted_rand_score
+from sklearn.feature_extraction import image
 
 from PIL import Image
 from torchvision.transforms import ToTensor
 
+import csv
+from random import randint
 
 torch.backends.cudnn.benchmark=True
 torch.backends.cudnn.enabled = False
@@ -24,60 +27,109 @@ torch.backends.cudnn.enabled = False
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 DATA_PATH = r'F:\Research Database\Deep_Neural_Networks_Segment_Neuronal_Membranes_in_Electorn_Microscopy_Images'
+PATCH_PATH = './image_patch'
 MODEL_STORE_PATH = r'F:\Research Database\Deep_Neural_Networks_Segment_Neuronal_Membranes_in_Electorn_Microscopy_Images\pytorch_models'
 
-train_image = io.imread(os.path.join(DATA_PATH, 'train-volume.tif'))
-label_image = io.imread(os.path.join(DATA_PATH, 'train-labels.tif'))
-test_image = io.imread(os.path.join(DATA_PATH, 'test-volume.tif'))
+train_image = skimage.io.imread(os.path.join(DATA_PATH, 'train-volume.tif'), plugin='tifffile')
+label_image = skimage.io.imread(os.path.join(DATA_PATH, 'train-labels.tif'), plugin='tifffile')
+test_image = skimage.io.imread(os.path.join(DATA_PATH, 'test-volume.tif'), plugin='tifffile')
+
+window_size = 65
+n_picture = 30
+new_dataset = 0
+if (new_dataset == 1):
+    f = open('data_csv.csv', 'w', encoding='utf-8', newline='')
+    wr = csv.writer(f)
+    for pic in range(n_picture):
+        patches_train = image.extract_patches_2d(train_image[pic], (window_size, window_size))
+        patches_label = image.extract_patches_2d(label_image[pic], (window_size, window_size))
+        for i in range(1000):
+            rand_int = randint(0, patches_train.shape[0]-1)
+            patch_filename = "./image_patch/" + str(pic*1000+i) + ".tif"
+            imsave(patch_filename, patches_train[rand_int])
+            wr.writerow([pic*1000+i, int(patches_label[rand_int,int((window_size-1)/2),int((window_size-1)/2)]/255)])
+    f.close()
 
 
-def extract_patches_2D(img, size):
-
-
-
-class N4model(nn.Module):
+class MemDataset(torch.utils.data.Dataset):
     def __init__(self):
-        super(N4model, self).__init__()
+        xy = np.loadtxt('data_csv.csv', delimiter=',', dtype = np.int64)
+        self.length = xy.shape[0]
+        self.x_data = torch.from_numpy(xy[:, 0:-1])
+        self.y_data = torch.from_numpy(xy[:, [-1]])
+        self.transform = transforms.Compose([transforms.ToTensor()])
+    def __getitem__(self, index):
+        b = self.x_data[index].numpy()[0]
+
+        patch_filename = "./image_patch/" + str(b) + ".tif"
+        image = Image.open(patch_filename)
+        image = self.transform(image)
+        label = self.y_data[index]
+        return image, label
+    def __len__(self):
+
+
+        return self.length
+
+# Hyperparameters
+num_epochs = 100
+num_classes = 2
+batch_size = 64
+learning_rate = 0.001
+
+train_dataset = MemDataset()
+train_loader = DataLoader(dataset=train_dataset, batch_size = batch_size, shuffle=True, num_workers=0)
+
+
+'''
+labels_map = {0 : 'mem', 1 : 'non-mem'};
+
+fig = plt.figure(figsize=(8,8));
+columns = 4;
+rows = 5;
+print(train_dataset)
+for i in range(1, columns*rows +1):
+    img_xy = np.random.randint(len(train_dataset));
+    img = train_dataset[img_xy][0]
+    fig.add_subplot(rows, columns, i)
+    plt.title(labels_map[int(train_dataset[img_xy][1].numpy()[0])])
+    plt.axis('off')
+    plt.imshow(img, cmap='gray')
+plt.show()
+'''
+
+
+
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 48, kernel_size=4,stride = 2, padding = 0),
+            nn.Conv2d(1, 16, kernel_size=5),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride = 2, padding=2),
-        )
+            nn.MaxPool2d(kernel_size=2, stride=2)).to(device)
         self.layer2 = nn.Sequential(
-            nn.Conv2d(48, 48, kernel_size = 5, stride = 1, padding = 0),
+            nn.Conv2d(16, 32, kernel_size=4),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride = 2, padding=0),
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(48, 48, kernel_size = 4, stride = 1, padding = 0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride = 2, padding=0),
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(48, 48, kernel_size = 4, stride = 1, padding = 0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride = 2, padding=0),
-        )
+            nn.MaxPool2d(kernel_size=2, stride=2)).to(device)
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(5408, 200)
+        self.fc2 = nn.Linear(200, 2)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
         out = out.reshape(out.size(0), -1)
         out = self.drop_out(out)
         out = self.fc1(out)
         out = self.fc2(out)
-        out = self.fc3(out)
-        out = self.fc4(out)
         return out
 
 
-
-model = N4model
-
+model = ConvNet()
 
 model = model.to(device)
+
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -89,12 +141,14 @@ loss_list = []
 acc_list = []
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-
         images = images.to(device)
         labels = labels.to(device)
 
+
         # Run the forward pass
         outputs = model(images)
+       
+        labels = labels.squeeze_()
         loss = criterion(outputs, labels)
         loss_list.append(loss.item())
 
@@ -109,18 +163,8 @@ for epoch in range(num_epochs):
         correct = (predicted == labels).sum().item()
         acc_list.append(correct / total)
 
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
+                          (correct / total) * 100))
 
-# Test the model
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-
-        images = images.to(device)
-        labels = labels.to(device)
-
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
